54daea7f0eaaa884314aeade4e0992412bec4f83
Alexey Tomilov <apofeosz@yandex.ru>
2021-09-19 05:05:35 +0600

    Updated the ::Api::V1::CryptocurrenciesController

--- api/app/controllers/api/v1/cryptocurrencies_controller.rb
+++ api/app/controllers/api/v1/cryptocurrencies_controller.rb
@@ -23,19 +23,31 @@ module Api
       end
 
       def convert_currencies
-        converter_result = ::Cryptocurrencies::Converters::SimpleConverter.new(
-          source_currency: params[:source_currency],
-          target_currency: params[:target_currency]
-        ).call
-        json_response(
-          object: converter_result.success? ? converter_result.value! : { errors: converter_result.failure }
+        render_monads_result(
+          monads_result: ::Cryptocurrencies::Converters::SimpleConverter.new(
+            **params.slice(*%i[source_currency target_currency]).permit!.to_h
+          ).call
+        )
+      end
+
+      def convert_currencies_via_additional_currency
+        render_monads_result(
+          monads_result: ::Cryptocurrencies::Converters::ComplexConverter.new(
+            **params.slice(*%i[source_currency target_currency broker_currency]).permit!.to_h
+          ).call
         )
       end
 
       private
 
       JOIN_DELIMITER = ', '.freeze
-      FIELDS_TO_VALIDATE = %i[currencies per_page page_number display_fields].freeze
+      FIELDS_TO_VALIDATE = %i[currencies per_page page_number display_fields broker_currency].freeze
+
+      def render_monads_result(monads_result:)
+        json_response(
+          object: monads_result.success? ? monads_result.value! : { errors: monads_result.failure }
+        )
+      end
 
       def prepared_array_of_hashes(array:, keys:)
         ::ObjectFilterers::Hashes::SliceKeys.in_array_of_hashes(array: array, keys: keys)
