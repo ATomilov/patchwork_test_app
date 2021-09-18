294fe933eec7fade303a6cde3b42a3d331b4bd10
Alexey Tomilov <apofeosz@yandex.ru>
2021-09-19 05:04:59 +0600

    Updated the ::Cryptocurrencies::Nomics::ConvertCurrenciesContract

--- api/app/contracts/cryptocurrencies/nomics/convert_currencies_contract.rb
+++ api/app/contracts/cryptocurrencies/nomics/convert_currencies_contract.rb
@@ -1,8 +1,6 @@
 module Cryptocurrencies
   module Nomics
     class ConvertCurrenciesContract < ::Cryptocurrencies::Nomics::BaseContract
-      WITHOUT_SPACES_REGEXP = /^\S+$/
-
       params do
         required(:source_currency).value(:string, format?: WITHOUT_SPACES_REGEXP)
         required(:target_currency).value(:string, format?: WITHOUT_SPACES_REGEXP)
