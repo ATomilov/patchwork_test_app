8df692661ed8d67f4f631db0b9e46a90ebbc975e
Alexey Tomilov <apofeosz@yandex.ru>
2021-09-19 03:41:12 +0600

    Updated routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :cryptocurrencies, only: %i[index]
+      resources :cryptocurrencies, only: %i[index] do
+        collection do
+          get :convert_currencies
+        end
+      end
     end
   end
 end
