61260b31a1075e691c858395d9c69c17ba123675
Alexey Tomilov <apofeosz@yandex.ru>
2021-09-18 23:26:02 +0600

    Updated the App Configurable

--- api/config/app.rb
+++ api/config/app.rb
@@ -1,14 +1,17 @@
 class App
   extend Dry::Configurable
 
-  # rubocop:disable Metrics/BlockLength
   setting :api do
     setting :v1 do
-      setting :currencies_ticker do
-        setting :path, default: 'currencies/ticker'
-        setting :request_method, default: 'get'
+      setting :nomics do
+        setting :base_url, default: 'https://api.nomics.com/v1/'
+        setting :currencies_ticker do
+          setting :path, default: 'currencies/ticker'
+          setting :request_method, default: 'get'
+        end
+        setting :per_page, default: 100
+        setting :page_number, default: 1
       end
     end
   end
-  # rubocop:enable Metrics/BlockLength
 end
